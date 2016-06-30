35024b176d81762f4824208188043791be51b736
zhangyunrui <635798274@qq.com>
2016-06-30 18:38:41 +0800

    class:1,2

--- class/changing_the_string_representation_of_instances.py
+++ class/changing_the_string_representation_of_instances.py
@@ -5,9 +5,13 @@ class Pair:
 	def __repr__(self):
 		return 'Pair({0.x!r}, {0.y!r})'.format(self)
 	def __str__(self):
-		return '({0.x!r}, {0.y!r})'.format(self)
+		return '({0.x!s}, {0.y!s})'.format(self)
 
 class basePair:
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
+
+# !r 格式化代码指明输出使用 __repr__() 来代替默认的 __str__() 。
+
+
