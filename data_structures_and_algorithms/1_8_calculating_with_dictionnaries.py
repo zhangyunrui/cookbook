480a3a8986329684aecac47540eca8eb2787e620
zhangyunrui <635798274@qq.com>
2016-07-01 18:09:36 +0800

    very much

---
+++ data_structures_and_algorithms/1_8_calculating_with_dictionnaries.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+prices = {                         
+    'ACME': 45.23,                 
+    'AAPL': 612.78,                
+    'IBM': 205.55,                 
+    'HPQ': 37.20,                  
+    'FB': 10.75                    
+}                                  
+                                   
+zip(prices.values(), prices.keys())
